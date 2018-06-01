50f036f35279dc1af0f7938405733aebd8223df8
DarkKnightof1 <mischa@boskat.eu>
2018-06-01 13:21:10 +0200

    Deel 2 part 1

---
+++ dbh.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: misch
+ * Date: 1-6-2018
+ * Time: 13:15
+ */
+$conn = mysqli_connect("localhost", "root", "root", "challengephpdatabase")
+?>
---
+++ re.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: misch
+ * Date: 1-6-2018
+ * Time: 10:24
+ */
+
+include 'dbh.php';
+
+$first = $_POST['first'];
+$last = $_POST['last'];
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+
+echo $first."<br>";
+echo $last."<br>";
+echo $uid."<br>";
+echo $pwd."<br>";
+
+
+?>
+
+
+<!--
+* naam: <Mischa Prudon>
+* groep: 7a
+* datum: 26-1-2018
+-->
