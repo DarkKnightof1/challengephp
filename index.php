01634ad8abfd00f5a7fcd6ae2cd1864047a30937
DarkKnightof1 <mischa@boskat.eu>
2018-06-01 09:11:49 +0200

    test

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    echo 'Test';
+/**
+ * Created by PhpStorm.
+ * User: misch
+ * Date: 1-6-2018
+ * Time: 08:32
+ */
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php phpinfo(); ?>
+/**
+ * Created by PhpStorm.
+ * User: misch
+ * Date: 1-6-2018
+ * Time: 08:37
+ */
